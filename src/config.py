60a071c65ac0f3d398e78dabb0f02f437030633a
root <gustavosecardovelli@gmail.com>
2025-02-24 20:01:45 +0000

    fix: internal db_port

--- src/config.py
+++ src/config.py
@@ -2,20 +2,20 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    DB_USER: str = "dev123"
-    DB_PASSWORD: str = "dev123"
-    DB_HOST: str = "dbfinance"
-    DB_NAME: str = "finance"
-    MYSQL_ROOT_PASSWORD: str = "1234"
-    MYSQL_DATABASE: str = "finance"
-    MYSQL_USER: str = "dev"
-    MYSQL_PASSWORD: str = "1234"
+    DB_USER: str = 'dev'
+    DB_PASSWORD: str = 'dev123'
+    DB_HOST: str = 'dbfinance'
+    DB_NAME: str = 'finance'
+    MYSQL_ROOT_PASSWORD: str = 'dev123'
+    MYSQL_DATABASE: str = 'finance'
+    MYSQL_USER: str = 'dev'
+    MYSQL_PASSWORD: str = 'dev123'
     DB_PORT: int = 3307
-    API_KEY: str = "apikeydev"
+    API_KEY: str = 'apikeydev'
 
     class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
 
     def get_db_url_alembic(self) -> str:
-        return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f'mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
