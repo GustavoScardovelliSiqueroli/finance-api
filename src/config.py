077a3d3abf302c709b654c6b3baa8bd87c37e22a
root <gustavosecardovelli@gmail.com>
2025-02-24 19:45:30 +0000

    feat: db connection

---
+++ src/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings
+
+
+class Config(BaseSettings):
+    DB_USER: str = "dev123"
+    DB_PASSWORD: str = "dev123"
+    DB_HOST: str = "dbfinance"
+    DB_NAME: str = "finance"
+    MYSQL_ROOT_PASSWORD: str = "1234"
+    MYSQL_DATABASE: str = "finance"
+    MYSQL_USER: str = "dev"
+    MYSQL_PASSWORD: str = "1234"
+    DB_PORT: int = 3307
+    API_KEY: str = "apikeydev"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+    def get_db_url_alembic(self) -> str:
+        return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
---
+++ src/infra/db/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import registry
+
+from src.config import Config
+
+config = Config()
+
+async_connection_string = f'mysql+aiomysql://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}'
+
+async_engine = create_async_engine(async_connection_string, echo=True)
+
+async_session = async_sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
+table_registry = registry()
