ad33f4594967e87469f27f37bd5e3b241761a0c7
root <gustavosecardovelli@gmail.com>
2025-02-25 17:28:52 +0000

    fix: unique to users login

--- src/domain/models/user.py
+++ src/domain/models/user.py
@@ -11,8 +11,8 @@ from src.domain.models.base import Base
 
 class User(Base):
     __tablename__ = 'users'
-    id: Mapped[UUID] = mapped_column(UUIDdb, primary_key=True, default=uuid4())
-    login: Mapped[str] = mapped_column(String(100), nullable=False)
+    id: Mapped[UUID] = mapped_column(UUIDdb, primary_key=True, default=uuid4)
+    login: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(256), nullable=False)
     refresh_token: Mapped[Optional[str]] = mapped_column(String(256), nullable=True)
     created_at: Mapped[datetime] = mapped_column(
