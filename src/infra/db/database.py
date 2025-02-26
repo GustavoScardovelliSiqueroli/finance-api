13ccf45da15751150b727d80a80d9c004899161d
root <gustavosecardovelli@gmail.com>
2025-02-26 20:07:36 +0000

    resource: ruff format apply

--- src/infra/db/database.py
+++ src/infra/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import registry
 
 from src.config import Config
 
-config = Config()
+config = Config()  # type: ignore
 
 async_connection_string = f'mysql+aiomysql://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}'
 
--- src/infra/db/scripts/verify_db_connection.py
+++ src/infra/db/scripts/verify_db_connection.py
@@ -3,38 +3,38 @@ import asyncio
 
 import aiomysql
 
-DB_HOST = "dbfinance"
+DB_HOST = 'dbfinance'
 DB_PORT = 3306
-DB_USER = "dev"
-DB_PASSWORD = "dev123"
-DB_NAME = "finance"
+DB_USER = 'dev'
+DB_PASSWORD = 'dev123'
+DB_NAME = 'finance'
 
 
 async def test_db_connection() -> None:
     try:
-        print("Tentando conectar ao banco de dados...")
+        print('Tentando conectar ao banco de dados...')
         conn = await aiomysql.connect(
             host=DB_HOST, port=DB_PORT, user=DB_USER, password=DB_PASSWORD, db=DB_NAME
         )
-        print("Conexão com o banco de dados bem-sucedida!")
+        print('Conexão com o banco de dados bem-sucedida!')
 
         async with conn.cursor() as cur:
-            await cur.execute("SHOW DATABASES;")
+            await cur.execute('SHOW DATABASES;')
             databases = await cur.fetchall()
-            print("Bases de dados disponíveis:")
+            print('Bases de dados disponíveis:')
             for db in databases:
-                print(f" - {db[0]}")
-                await cur.execute(f"USE {db[0]};")
-                await cur.execute("SHOW TABLES;")
+                print(f' - {db[0]}')
+                await cur.execute(f'USE {db[0]};')
+                await cur.execute('SHOW TABLES;')
                 tables = await cur.fetchall()
                 for table in tables:
-                    print(f"   - {table[0]}")
+                    print(f'   - {table[0]}')
 
         conn.close()
 
     except Exception as e:
-        print(f"Erro ao conectar ao banco de dados: {e}")
+        print(f'Erro ao conectar ao banco de dados: {e}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(test_db_connection())
