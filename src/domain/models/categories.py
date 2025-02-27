bc25d1269528c7c4db08e9d5fc73ea5f0dc91670
root <gustavosecardovelli@gmail.com>
2025-02-27 18:25:51 +0000

    feat: relationship between categories and transactions

--- src/domain/models/categories.py
+++ src/domain/models/categories.py
@@ -1,1 +1,32 @@
-class Category: ...
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from sqlalchemy import UUID as UUIDdb
+from sqlalchemy import DateTime, Enum, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.domain.models.base import Base
+from src.domain.models.enums.type_enum import Type
+from src.domain.models.transaction import Transaction
+from src.domain.models.transactions_categories import TransactionCategory
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id_user: Mapped[UUID] = mapped_column(UUIDdb, nullable=False, foreignKey='users.id')
+    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    type: Mapped[Type] = mapped_column(Enum(Type), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=datetime.now()
+    )
+    updated_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
+    deleted_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
+
+    transaction_categories: Mapped[list['TransactionCategory']] = relationship(
+        'TransactionCategory', back_populates='category'
+    )
+    transactions: Mapped[list['Transaction']] = relationship(
+        'Transaction', secondary='transaction_categories', back_populates='categories'
+    )
--- src/domain/models/transaction.py
+++ src/domain/models/transaction.py
@@ -1,18 +1,20 @@
 from datetime import datetime
 from typing import Optional
-from uuid import UUID, uuid4
+from uuid import UUID
 
 from sqlalchemy import UUID as UUIDdb
-from sqlalchemy import DateTime, Enum, Numeric, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, Enum, Integer, Numeric, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.domain.models.base import Base
+from src.domain.models.categories import Category
 from src.domain.models.enums.type_enum import Type
+from src.domain.models.transactions_categories import TransactionCategory
 
 
 class Transaction(Base):
     __tablename__ = 'transactions'
-    id: Mapped[UUID] = mapped_column(UUIDdb, primary_key=True, default=uuid4)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     id_user: Mapped[UUID] = mapped_column(UUIDdb, nullable=False, foreignKey='users.id')
     value: Mapped[float] = mapped_column(Numeric(15, 2), nullable=False)
     description: Mapped[Optional[str]] = mapped_column(String(100), nullable=False)
@@ -23,6 +25,9 @@ class Transaction(Base):
     updated_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
     deleted_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
 
-    def __init__(self, login: str, password: str) -> None:
-        self.login = login
-        self.password = password
+    transaction_categories: Mapped[list['TransactionCategory']] = relationship(
+        'TransactionCategory', back_populates='transaction'
+    )
+    categories: Mapped[list['Category']] = relationship(
+        'Category', secondary='transaction_categories', back_populates='transactions'
+    )
